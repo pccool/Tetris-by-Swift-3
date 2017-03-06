2cc27f4c1997a72ff9fb197b6c747552419ceebc
coolFog <pccool@me.com>
2017-03-06 21:10:21 +0800

    初始化项目

---
+++ Tetris by Swift 3/Tetris by Swift 3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tetris by Swift 3
+//
+//  Created by 赵国欣 on 17/3/6.
+//  Copyright © 2017年 coolFog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Tetris by Swift 3/Tetris by Swift 3Tests/Tetris_by_Swift_3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Tetris_by_Swift_3Tests.swift
+//  Tetris by Swift 3Tests
+//
+//  Created by 赵国欣 on 17/3/6.
+//  Copyright © 2017年 coolFog. All rights reserved.
+//
+
+import XCTest
+@testable import Tetris_by_Swift_3
+
+class Tetris_by_Swift_3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
